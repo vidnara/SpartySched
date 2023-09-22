213d3e1b25446fb158178cc15fd6ebe52c670d70
vidnara <145811471+vidnara@users.noreply.github.com>
2023-09-22 15:37:46 -0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\vidhya\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\vidhya\\IdeaProjects\\SpartySched" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\vidhya\\Downloads\\flutter"
+  "PROJECT_DIR=C:\\Users\\vidhya\\IdeaProjects\\SpartySched"
+  "FLUTTER_ROOT=C:\\Users\\vidhya\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\vidhya\\IdeaProjects\\SpartySched\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\vidhya\\IdeaProjects\\SpartySched"
+  "FLUTTER_TARGET=C:\\Users\\vidhya\\IdeaProjects\\SpartySched\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\vidhya\\IdeaProjects\\SpartySched\\.dart_tool\\package_config.json"
+)
